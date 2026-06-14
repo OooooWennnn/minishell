1ac58ade6ada7d7584414035f3edc3b595f723a5
Sumin Han <142552355+OooooWennnn@users.noreply.github.com>
2026-06-14 07:37:21 +0000

    Implement env built-in command

--- src/builtins/builtin_env.c
+++ src/builtins/builtin_env.c
@@ -2,8 +2,17 @@
 #include <stdio.h>
 
 int builtin_env(char **args, t_env **env_list) {
-    
+    if (args[1] != NULL) {
+        fprintf(stderr, "env : options are not supported\n");
+        return 1;
+    }
 
-    printf("builtin_env function called\n");
-    return 1;
+    t_env *curr = *env_list;
+    while (curr != NULL) {
+        if (curr->value != NULL) {
+            printf("%s=%s\n", curr->key, curr->value);
+        }
+        curr = curr->next;
+    }
+    return 0;
 }
